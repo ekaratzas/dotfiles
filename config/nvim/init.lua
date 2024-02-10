5272841bbdf05a8cf296b6bc53cb58676a0a60c3
Efstratios Karatzas <gpf.kira@gmail.com>
2024-02-09 23:17:12 -0800

    add colorschemes and split them off to files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,12 +7,16 @@ vim.api.nvim_command([[
         Plug 'junegunn/fzf.vim'
         Plug 'itchyny/lightline.vim'
         Plug 'bfrg/vim-cpp-modern'
-        Plug 'catppuccin/nvim'
         Plug 'lukas-reineke/indent-blankline.nvim'
         Plug 'nvim-tree/nvim-web-devicons'
         Plug 'nvimdev/dashboard-nvim'
         Plug 'romgrk/barbar.nvim'
         Plug 'kana/vim-smartword'
+        Plug 'catppuccin/nvim'
+        Plug 'neanias/everforest-nvim'
+        Plug 'rebelot/kanagawa.nvim'
+        Plug 'rmehri01/onenord.nvim'
+        Plug 'rose-pine/neovim'
     call plug#end()
 ]])
 

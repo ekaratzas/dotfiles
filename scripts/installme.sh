c3e877c7ec609017a6b7ab39d9ecfb6c324cdebc
Efstratios Karatzas <gpf.kira@gmail.com>
2026-01-09 22:41:18 -0800

    enable zsh fzf history search

--- scripts/installme.sh
+++ scripts/installme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y 7zip zip unzip p7zip tar mousepad vlc mpv build-essential python3 git ranger kolourpaint qbittorrent musescore rofi silversearcher-ag zsh command-not-found zsh-autosuggestions zsh-syntax-highlighting flatpak ethtool net-tools htop neofetch xfce4-appfinder xsel wget vim
+sudo apt install -y 7zip zip unzip p7zip tar mousepad vlc mpv build-essential python3 git ranger kolourpaint qbittorrent musescore rofi silversearcher-ag zsh fzf command-not-found zsh-autosuggestions zsh-syntax-highlighting flatpak ethtool net-tools htop neofetch xfce4-appfinder xsel wget vim

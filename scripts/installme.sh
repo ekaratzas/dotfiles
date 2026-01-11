735196db3e92d428465caa90eca8be98388b67e4
Efstratios Karatzas <gpf.kira@gmail.com>
2026-01-10 23:10:47 -0800

    add stow

--- scripts/installme.sh
+++ scripts/installme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y 7zip zip unrar-free unzip p7zip tar mousepad vlc mpv build-essential python3 git ranger kolourpaint qbittorrent musescore rofi silversearcher-ag zsh fzf command-not-found zsh-autosuggestions zsh-syntax-highlighting flatpak ethtool net-tools htop neofetch xfce4-appfinder xsel wget vim bat
+sudo apt install -y 7zip zip unrar-free unzip p7zip tar mousepad vlc mpv build-essential python3 git ranger kolourpaint qbittorrent musescore rofi silversearcher-ag zsh fzf command-not-found zsh-autosuggestions zsh-syntax-highlighting flatpak ethtool net-tools htop neofetch xfce4-appfinder xsel wget vim bat stow
